0b2a227153aadd71f8523604f4121af748c36fc3
Jeffrey Elkner <jeff@elkner.net>
2017-03-05 14:13:13 -0500

    Added function to test runtime for two other functions

--- ch2_algorithm_analysis/time_tests.py
+++ ch2_algorithm_analysis/time_tests.py
@@ -2,17 +2,36 @@
 import time
 
 
-def sum_of_n_2(n):
+def sum_of_n(n):
     start = time.time()
 
     the_sum = 0
     for i in range(1, n+1):
-        the_sum += 1
+        the_sum += i
 
     end = time.time()
 
     return the_sum, end - start
 
 
-for i in range(10):
-    print("Sum is {0} required {1} seconds".format(*sum_of_n_2(10000)))
+def sum_of_n2(n):
+    start = time.time()
+
+    the_sum = (n * (n + 1)) / 2
+
+    end = time.time()
+
+    return the_sum, end - start
+
+
+def test_runtime(f):
+    val = 10000
+    for i in range(3):
+        for i in range(5):
+            print("Sum is {0} required {1} seconds".format(*f(val)))
+        print()
+        val *= 10
+
+
+test_runtime(sum_of_n)
+test_runtime(sum_of_n2)
